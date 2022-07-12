63a9722cdb4c2fc21f1508b162dea7a2dc94066a
Natasha0611 <lunataly06@gmail.com>
2022-07-12 20:13:14 +0100

    Showing current weather

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function displayTemperature(response){
+    console.log(response.data);
+let temperatureElement = document.querySelector(`#temperature`);
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+let cityElement = document.querySelector(`#city`);
+cityElement.innerHTML = response.data.name;
+let descriptionElement = document.querySelector(`#description`);
+descriptionElement.innerHTML = response.data.weather[0].description;
+let humidityElement = document.querySelector(`#humidity`);
+humidityElement.innerHTML = response.data.main.humidity;
+let windElement = document.querySelector(`#wind`);
+windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = `907fef83425ee6575ce345a2f87d8989`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
